893fb1b4e2f08e149a3b1e86fb130a339fad0cfd
jzybert <jmzybert@gmail.com>
2018-11-17 18:21:20 -0500

    Add new earthquake query view

--- lib/earthquake_tracker_web/controllers/earthquake_query_controller.ex
+++ lib/earthquake_tracker_web/controllers/earthquake_query_controller.ex
@@ -1,6 +1,10 @@
 defmodule EarthquakeTrackerWeb.EarthquakeQueryController do
   use EarthquakeTrackerWeb, :controller
 
+  def index(conn, _params) do
+    render conn, "index.html"
+  end
+
   def query_earthquake(conn, _params) do
     url = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2014-01-01&endtime=2014-01-02&minmagnitude=5"
     headers = []
