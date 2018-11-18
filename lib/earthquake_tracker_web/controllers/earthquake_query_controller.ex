a420f8d42b5aeb3069e081d751d8b70ec02130dd
jzybert <jmzybert@gmail.com>
2018-11-17 20:06:24 -0500

    Add code to query API

--- lib/earthquake_tracker_web/controllers/earthquake_query_controller.ex
+++ lib/earthquake_tracker_web/controllers/earthquake_query_controller.ex
@@ -5,16 +5,31 @@ defmodule EarthquakeTrackerWeb.EarthquakeQueryController do
     render(conn, "index.html")
   end
 
-  def query_earthquake(conn, params) do
-    IO.puts inspect(params)
-    url = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2014-01-01&endtime=2014-01-02&minmagnitude=5"
+  def query_earthquake(conn, %{"start_time" => start_time,
+      "end_time" => end_time, "location" => location, "sq_min_lat" => sq_min_lat,
+      "sq_max_lat" => sq_max_lat, "sq_min_lng" => sq_min_lng, "sq_max_lng" => sq_max_lng,
+      "ci_lat" => ci_lat, "start_time" => ci_lng, "ci_max_rad" => ci_max_rad,}) do
+    url = base_string() <> add_time(start_time, end_time)
+    url =
+      if location == "square" do
+        url <> add_square(sq_min_lat, sq_max_lat, sq_min_lng, sq_max_lng)
+      else
+        if location == "circle" do
+          url <> add_circle(ci_lat, ci_lng, ci_max_rad)
+        else
+          url
+        end
+      end
     headers = []
     params = []
 
-    case HTTPoison.get(url, headers, params: params) do
-      {:ok, response} -> parse_eq_data(response)
-      {:error, reason} -> IO.puts inspect(reason)
-    end
+    parsed =
+      case HTTPoison.get(url, headers, params: params) do
+        {:ok, response} -> parse_eq_data(response)
+        {:error, reason} -> %{}
+      end
+
+    IO.puts inspect(parsed)
 
     conn
     |> redirect(to: Routes.page_path(conn, :index))
@@ -25,8 +40,22 @@ defmodule EarthquakeTrackerWeb.EarthquakeQueryController do
     data_part = elem(decoded_data, 1)
     metadata = Map.get(data_part, "metadata")
     features = Map.get(data_part, "features")
-    #IO.puts inspect(metadata)
-    #IO.puts inspect(features)
+    %{"metadata": metadata, "features": features}
+  end
+
+  defp base_string(), do: "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson"
+
+  defp add_time(start_time, end_time) do
+    "&starttime=" <> start_time <> "&endtime=" <> end_time
+  end
+
+  defp add_square(min_lat, max_lat, min_lng, max_lng) do
+    "&minlatitude=" <> min_lat <> "&maxlatitude=" <> max_lat
+    <> "&minlongitude=" <> min_lng <> "&maxlongitude=" <> max_lng
+  end
+
+  defp add_circle(lat, lng, rad) do
+    "&latitude=" <> lat <> "&longitude=" <> lng <> "&maxradius=" <> rad
   end
 end
 
