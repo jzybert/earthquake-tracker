deef699bdec5a21eddf795bd0a86ce36fe1ca358
Juan Lopez <lopez.ju@husky.neu.edu>
2018-11-22 15:43:47 -0500

    getting rid of unused module

--- lib/earthquake_tracker/earthquake_query/earthquake_query.ex
+++
@@ -1,4 +0,0 @@
-defmodule EarthquakeTracker.EarthquakeQuery.EarthquakeQuery do
-  defstruct [:start_time, :end_time, :location, :sq_min_lat, :sq_max_lat,
-             :sq_min_lng, :sq_max_lng, :ci_lat, :ci_lng, :ci_max_rad]
-end
