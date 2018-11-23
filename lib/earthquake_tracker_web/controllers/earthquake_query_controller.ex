c49697b770bd2e572b1078152e86c3a7c2dc7ef6
jzybert <jmzybert@gmail.com>
2018-11-23 11:11:09 -0800

    Send emails only if user is opted-in

--- lib/earthquake_tracker_web/controllers/earthquake_query_controller.ex
+++ lib/earthquake_tracker_web/controllers/earthquake_query_controller.ex
@@ -12,16 +12,21 @@ defmodule EarthquakeTrackerWeb.EarthquakeQueryController do
     render(conn, "show.html", eq_data: eq_data, num_eq: num_eq, start_time: start_time, end_time: end_time)
   end
 
-  def send_email(conn, %{"email_address" => email_address}) do
-    try do
-      Email.tracked_area_email(email_address) |> Mailer.deliver_now
+  def send_email(conn, %{"email_address" => email_address, "opted_in" => opted_in}) do
+    if opted_in == "true" do
+      try do
+        Email.tracked_area_email(email_address) |> Mailer.deliver_now
+        conn
+        |> redirect(to: Routes.page_path(conn, :index))
+      rescue
+        e in Bamboo.SMTPAdapter.SMTPError ->
+          conn
+          |> put_flash(:error, "Failed to send email via AWS.")
+          |> redirect(to: Routes.page_path(conn, :index))
+      end
+    else
       conn
       |> redirect(to: Routes.page_path(conn, :index))
-    rescue
-      e in Bamboo.SMTPAdapter.SMTPError ->
-        conn
-        |> put_flash(:error, "Failed to send email via AWS.")
-        |> redirect(to: Routes.page_path(conn, :index))
     end
   end
 
